e939fb0dfeb6cd65456f69c2b277ec5539633a55
Sahas Punchihewa <sahasmcg2995@gmail.com>
2022-08-20 08:46:11 +0530

    Reindented

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 
 import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss";
@@ -31,22 +31,22 @@ import ProfilePage from "views/examples/ProfilePage.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/components" render={(props) => <Index {...props} />} />
-      <Route
-        path="/landing-page"
-        render={(props) => <LandingPage {...props} />}
-      />
-      <Route
-        path="/register-page"
-        render={(props) => <RegisterPage {...props} />}
-      />
-      <Route
-        path="/profile-page"
-        render={(props) => <ProfilePage {...props} />}
-      />
-      <Redirect from="/" to="/components" />
-    </Switch>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Switch>
+            <Route path="/components" render={(props) => <Index {...props} />}/>
+            <Route
+                path="/landing-page"
+                render={(props) => <LandingPage {...props} />}
+            />
+            <Route
+                path="/register-page"
+                render={(props) => <RegisterPage {...props} />}
+            />
+            <Route
+                path="/profile-page"
+                render={(props) => <ProfilePage {...props} />}
+            />
+            <Redirect from="/" to="/components"/>
+        </Switch>
+    </BrowserRouter>
 );
--- src/views/Index.js
+++ src/views/Index.js
@@ -36,33 +36,33 @@ import Examples from "views/IndexSections/Examples.js";
 import Download from "views/IndexSections/Download.js";
 
 export default function Index() {
-  React.useEffect(() => {
-    document.body.classList.toggle("index-page");
-    // Specify how to clean up after this effect:
-    return function cleanup() {
-      document.body.classList.toggle("index-page");
-    };
-  }, []);
-  return (
-    <>
-      <IndexNavbar />
-      <div className="wrapper">
-        <PageHeader />
-        <div className="main">
-          <Basics />
-          <Navbars />
-          <Tabs />
-          <Pagination />
-          <Notifications />
-          <Typography />
-          <JavaScript />
-          <NucleoIcons />
-          <Signup />
-          <Examples />
-          <Download />
-        </div>
-        <Footer />
-      </div>
-    </>
-  );
+    React.useEffect(() => {
+        document.body.classList.toggle("index-page");
+        // Specify how to clean up after this effect:
+        return function cleanup() {
+            document.body.classList.toggle("index-page");
+        };
+    }, []);
+    return (
+        <>
+            <IndexNavbar/>
+            <div className="wrapper">
+                <PageHeader/>
+                <div className="main">
+                    <Basics/>
+                    <Navbars/>
+                    <Tabs/>
+                    <Pagination/>
+                    <Notifications/>
+                    <Typography/>
+                    <JavaScript/>
+                    <NucleoIcons/>
+                    <Signup/>
+                    <Examples/>
+                    <Download/>
+                </div>
+                <Footer/>
+            </div>
+        </>
+    );
 }
